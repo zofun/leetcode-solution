8553767f4dfed8819561721c747949cc21fe0b33
tcw <1483674736@qq.com>
2019-07-06 19:05:05 +0800

    merge two sorted lists

---
+++ 21.merge-two-sorted-lists.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=21 lang=java
+ *
+ * [21] Merge Two Sorted Lists
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+
+        if (l1 == null){
+            return l2;
+        }
+
+        if (l2 == null){
+            return l1;
+        }
+ 
+        //使用递归的方式
+        ListNode head = null;
+        if (l1.val <= l2.val){
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
+}
+
