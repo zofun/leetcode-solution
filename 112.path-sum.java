e15cc2948126954bb56fed777488d3af0625bbeb
tcw <1483674736@qq.com>
2019-07-18 14:42:25 +0800

    path sum

---
+++ 112.path-sum.java
@@ -0,0 +1,31 @@
+
+
+/*
+ * @lc app=leetcode id=112 lang=java
+ *
+ * [112] Path Sum
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        //此事不简单
+        if(root == null ){
+            return false;
+        }
+        if (root.left == null && root.right == null && (sum - root.val) == 0) {
+            return true;
+        }
+      
+        return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+        
+    }
+}
+
