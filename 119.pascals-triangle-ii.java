6cac9b07a28da5f8606a7cf802a7ef970b00ffda
tcw <1483674736@qq.com>
2019-07-19 09:21:38 +0800

    pascals triangle ii

---
+++ 119.pascals-triangle-ii.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=119 lang=java
+ *
+ * [119] Pascal's Triangle II
+ */
+class Solution {
+
+    public List<Integer> getRow(int rowIndex) {
+
+    ArrayList<Integer> result = new ArrayList<Integer>();
+ 
+	    if (rowIndex < 0){
+            return result;
+         }
+	    result.add(1);
+	    for (int i = 1; i <= rowIndex; i++) {
+		    for (int j = result.size() - 2; j >= 0; j--) {
+			    result.set(j + 1, result.get(j) + result.get(j + 1));
+		    }
+		    result.add(1);
+        }
+    
+	    return result;  
+    }
+}
+
