6541f9e54094b48a98e4e29fa9e273ed2a261a2d
tcw <1483674736@qq.com>
2019-07-30 17:16:11 +0800

    Majority Element 值得好好学习

--- 169.majority-element.java
+++ 169.majority-element.java
@@ -9,6 +9,7 @@ class Solution {
     public int majorityElement(int[] nums) {
         //可以利用hashmao来实现
         //也可以使用多数投票算法,多数投票算法如下
+        //这道题值得好好学习
         int candidate=0;
         int count=0;
         for(int i=0;i<nums.length;i++){
