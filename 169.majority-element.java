2999727ffe0c49e23097183fb062f4abddc1b1d6
tcw <1483674736@qq.com>
2019-07-30 17:15:09 +0800

    Majority Element

---
+++ 169.majority-element.java
@@ -0,0 +1,32 @@
+
+
+/*
+ * @lc app=leetcode id=169 lang=java
+ *
+ * [169] Majority Element
+ */
+class Solution {
+    public int majorityElement(int[] nums) {
+        //可以利用hashmao来实现
+        //也可以使用多数投票算法,多数投票算法如下
+        int candidate=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                candidate=nums[i];
+            }
+
+            if(nums[i]==candidate){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        return candidate;
+        
+    }
+  
+
+}
+
