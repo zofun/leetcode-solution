7782575d656bef1ecd8c2fb8795d36fff10eee58
tcw <1483674736@qq.com>
2019-08-01 14:32:15 +0800

    Reverse Bits

---
+++ 190.reverse-bits.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=190 lang=java
+ *
+ * [190] Reverse Bits
+ */
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result=0;
+        for(int i=0;i<32;i++){
+            if((n&1)==1){
+                result=(result<<1)+1;
+            }else{
+                result=result<<1;
+            }
+            n=n>>1;
+        }
+        return result;
+        
+    }
+}
+
