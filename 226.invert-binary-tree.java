0a10946adec18abf916a45db25decb09b87d7dfc
tcw <1483674736@qq.com>
2019-08-03 09:55:56 +0800

    Invert Binary Tree 递归解法

---
+++ 226.invert-binary-tree.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=226 lang=java
+ *
+ * [226] Invert Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+
+        if(root==null){
+            return null;
+        }
+
+        TreeNode temp=root.left;
+        root.left=invertTree(root.right);
+        root.right=invertTree(temp);
+        return root;
+
+        
+    }
+}
+
