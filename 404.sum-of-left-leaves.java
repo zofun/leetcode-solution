f548099bfae7391afc31b7ce1381e3cb466b5b43
tcw <1483674736@qq.com>
2019-08-09 11:22:23 +0800

    Sum of Left Leaves 递归解法

---
+++ 404.sum-of-left-leaves.java
@@ -0,0 +1,32 @@
+
+
+/*
+ * @lc app=leetcode id=404 lang=java
+ *
+ * [404] Sum of Left Leaves
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+      if(root==null){
+          return 0;
+      }
+      if(root.left!=null&&root.left.left==null&&root.left.right==null){
+          return root.left.val+sumOfLeftLeaves(root.right);
+      }else{
+        return sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
+      }
+    }
+
+
+   
+}
+
