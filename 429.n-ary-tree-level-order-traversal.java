5ed8d91a78ce5e74864b8eb5e7ca19638c515d1b
tcw <1483674736@qq.com>
2019-08-10 17:14:00 +0800

    N-ary Tree Level Order Traversal

---
+++ 429.n-ary-tree-level-order-traversal.java
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=429 lang=java
+ *
+ * [429] N-ary Tree Level Order Traversal
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> llist = new ArrayList<>();
+        if (root == null){
+            return llist;
+        }
+        //使用队列，实现按层次遍历
+        ArrayDeque<Node> que = new ArrayDeque<>();
+        que.offer(root);
+        while (!que.isEmpty()){
+            int size = que.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; ++ i){
+                Node tmp = que.poll();
+                list.add(tmp.val);
+                for (Node node : tmp.children){
+                    if (node != null){
+                        que.offer(node);
+                    }
+                }
+            }
+            llist.add(list);
+        }
+        return llist;
+    }
+
+}
+
