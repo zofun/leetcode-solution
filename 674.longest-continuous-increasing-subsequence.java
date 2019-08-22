f2a190bb1d08a8e234196d2d71c88cf48d07fce6
tcw <1483674736@qq.com>
2019-08-22 11:38:26 +0800

    Longest Continuous Increasing Subsequence  值得学习

---
+++ 674.longest-continuous-increasing-subsequence.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=674 lang=java
+ *
+ * [674] Longest Continuous Increasing Subsequence
+ */
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int res = 0, cnt = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0 || nums[i-1] < nums[i]){
+                res = Math.max(res, ++cnt);
+            }else{
+                cnt = 1;
+            }
+
+        }
+        return res;
+    }
+}
+
