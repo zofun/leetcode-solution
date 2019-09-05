6ca0bc6704b3bf6937aa8403d640548ec0062d7e
tcw <1483674736@qq.com>
2019-09-05 21:31:37 +0800

    Middle of the Linked List 快慢指针

---
+++ 876.middle-of-the-linked-list.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=876 lang=java
+ *
+ * [876] Middle of the Linked List
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        //只有一个结点或为空
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+        
+    }
+}
+
