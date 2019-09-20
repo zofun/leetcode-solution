52b9cf56642403755a37228068942130274f8fb3
tcw <1483674736@qq.com>
2019-09-20 21:17:49 +0800

    Largest Perimeter Triangle 值得学习

---
+++ 976.largest-perimeter-triangle.java
@@ -0,0 +1,84 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=976 lang=java
+ *
+ * [976] Largest Perimeter Triangle
+ *
+ * https://leetcode.com/problems/largest-perimeter-triangle/description/
+ *
+ * algorithms
+ * Easy (56.89%)
+ * Likes:    201
+ * Dislikes: 25
+ * Total Accepted:    20.8K
+ * Total Submissions: 36.5K
+ * Testcase Example:  '[2,1,2]'
+ *
+ * Given an array A of positive lengths, return the largest perimeter of a
+ * triangle with non-zero area, formed from 3 of these lengths.
+ * 
+ * If it is impossible to form any triangle of non-zero area, return 0.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,1,2]
+ * Output: 5
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [1,2,1]
+ * Output: 0
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: [3,2,3,4]
+ * Output: 10
+ * 
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: [3,6,2,3]
+ * Output: 8
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 3 <= A.length <= 10000
+ * 1 <= A[i] <= 10^6
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        for(int i=A.length-3;i>=0;--i){
+            if(A[i]+A[i+1]>A[i+2]){
+                return A[i]+A[i+1]+A[i+2];
+            }
+        }
+        return 0;
+    }
+}
+
