82067c038d6ca24e03cd485b605319e9022cbd2f
tcw <1483674736@qq.com>
2019-09-26 21:33:17 +0800

    Divisor Game

---
+++ 1025.divisor-game.java
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=1025 lang=java
+ *
+ * [1025] Divisor Game
+ *
+ * https://leetcode.com/problems/divisor-game/description/
+ *
+ * algorithms
+ * Easy (65.14%)
+ * Likes:    153
+ * Dislikes: 447
+ * Total Accepted:    24.3K
+ * Total Submissions: 37.3K
+ * Testcase Example:  '2'
+ *
+ * Alice and Bob take turns playing a game, with Alice starting first.
+ * 
+ * Initially, there is a number N on the chalkboard.  On each player's turn,
+ * that player makes a move consisting of:
+ * 
+ * 
+ * Choosing any x with 0 < x < N and N % x == 0.
+ * Replacing the number N on the chalkboard with N - x.
+ * 
+ * 
+ * Also, if a player cannot make a move, they lose the game.
+ * 
+ * Return True if and only if Alice wins the game, assuming both players play
+ * optimally.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2
+ * Output: true
+ * Explanation: Alice chooses 1, and Bob has no more moves.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 3
+ * Output: false
+ * Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more
+ * moves.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= N <= 1000
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public boolean divisorGame(int N) {
+        return N%2==0;
+    }
+}
+
